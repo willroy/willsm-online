3e3f10ce978726984d1de421dafbcebe8f51d751
willroy <willjoroy@gmail.com>
2025-06-29 12:00:58 +0100

    looks like its working, add to vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,16 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                'resources/css/themes/newgeneration.css',
+                'resources/css/themes/classic.css',
+                'resources/css/themes/catppiccinlatte.css',
+                'resources/css/themes/oshawatt.css',
+                'resources/css/themes/base2tonedesert.css',
+                'resources/css/themes/pastelbonanza.css'
+            ],
             refresh: true,
         }),
     ],
